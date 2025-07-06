eea5d7407d92bf3047eef674077569db5c9e47f6
Glenn Tanze <71010568+G4EVA-dev@users.noreply.github.com>
2025-07-06 22:01:52 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'trafficAz',
+  description: 'TrafficAZ is an intelligent, voice-activated traffic monitoring and reporting application designed for drivers',
 }
 
 export default function RootLayout({
